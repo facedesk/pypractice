8c01a53bd3a9e0019331320c7a5332a43c6fd559
facedesk <dgold@sjnma.org>
2018-02-05 10:19:36 -0600

    learned string variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+
+
+print('''hello world!
+hello world!
+asdfas
+asdf
+asdf
+asddf 
+and this will show up!
+''')
+
+first_name = raw_input("what is your first name?")
+
+last_name = raw_input("what is your last name?")
+
+full_name = first_name +" "+ last_name
+
+print(full_name)
+
+
+
+
+
+
+
